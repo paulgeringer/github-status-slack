939611695a36c16c28f4b4bd54bbe002f4aa632d
Paul Geringer <paul.j.geringer@gmail.com>
2015-03-27 15:50:30 -0500

    Switch to rufus-scheduler, use thor.
    Rufus-scheduler has some nice behavior and is much easier to use than
    timers.
    Thor allows you to make some of the settings more reasonable from the
    command line.

--- github_status_slack.gemspec
+++ github_status_slack.gemspec
@@ -17,9 +17,10 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
-  spec.add_dependency "timers", '4.0.1'
+  spec.add_dependency "rufus-scheduler", '3.0.9'
   spec.add_dependency "json", '1.8.2'
   spec.add_dependency "slack-notifier", '1.1.0'
+  spec.add_dependency "thor", '0.19.1'
 
   spec.add_development_dependency "bundler", "~> 1.7"
   spec.add_development_dependency "rake", "~> 10.0"
--- lib/github_status_slack.rb
+++ lib/github_status_slack.rb
@@ -2,42 +2,38 @@ require 'time'
 require 'json'
 require 'slack-notifier'
 require 'net/http'
-require 'timers'
+require 'rufus-scheduler'
 
 require 'github_status_slack/github_status.rb'
 require 'github_status_slack/status_retriever.rb'
-require 'github_status_slack/slack_communicator.rb'
 require "github_status_slack/version"
 
 module GithubStatusSlack
   class Notifier
-    attr_accessor :interval, :timers, :status_retriever, :slack_notifier
-    def initialize( webhook_url, api_url, interval=10 )
-      @slack_notifier = GithubStatusSlack::SlackCommunicator.new(webhook_url).notifier
+    attr_accessor :interval, :interval_method, :scheduler, :status_retriever, :slack_notifier
+
+    def initialize( webhook_url, api_url, options={} )
+      @slack_notifier = Slack::Notifier.new( webhook_url, channels: options[:channels], username: options[:username] )
       @status_retriever = GithubStatusSlack::StatusRetriever.new(api_url)
-      @timers = Timers::Group.new
-      @interval = interval
+      @scheduler = Rufus::Scheduler.new
+      @interval_method = options[:interval_type] || 'every'
+      @interval = options[:interval] || '10s'
     end
 
-    def looper
-      puts "hi1"
+    def notify_slack 
       slack_notifier.ping "Tracking Github status..."
       old_status = GithubStatus.new(status_retriever.request)
       msg = "#{old_status.body} Posted @ #{old_status.time}."
       slack_notifier.ping msg
-      puts "hi2"
-      timers.every(interval) do
+      scheduler.send(interval_method.to_sym, interval) do
         current_status = GithubStatus.new(status_retriever.request)
-        puts "in the loop"
         if current_status.time > old_status.time
           msg = "#{current_status.body} Posted @ #{current_status.time}."
           slack_notifier.ping msg
         end
         old_status = current_status.dup
       end
-      loop do
-        timers.wait
-      end
+      scheduler.join
     end
   end
 
